e7051b69e13105883f5cc10b917ce669844ebaba
amit <gamesil456@gmail.com>
2021-01-05 20:42:50 +0200

    Fully fledged session persisting

--- src/App.js
+++ src/App.js
@@ -2,19 +2,69 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
 } from "react-router-dom";
+import HomePage from "./pages/home";
+import LoginPage from "./pages/login";
+import {useDispatch, useSelector} from 'react-redux';
+import { refreshToken } from "./state/actions/user.actions";
+import { useEffect } from "react";
 
-import LoginPage from './pages/login';
+export const PrivateRoute = ({ component: Component, ...rest }) => {
+  const dispatch = useDispatch();
+  const refreshAssets = useSelector((state) => state.refreshToken.loginAssets);
+  const refreshStatus = useSelector((state) => state.refreshToken.status);
+  const localRefreshToken = localStorage.getItem("refreshToken");
+
+  useEffect(() => {
+    console.log("YO BRO", refreshStatus);
+    if (refreshStatus === "success") {
+      localStorage.setItem("refreshToken", refreshAssets.refreshToken);
+      localStorage.setItem("accessToken", refreshAssets.accessToken);
+      localStorage.setItem("accessTokenExpiresOn", refreshAssets.expiresOn);
+
+    } else if (refreshStatus === "error") {
+      console.log("There's an error in refresh");
+      document.location.href = "/login";
+    }
+  }, [refreshStatus]);
+
+  let isTokenExpired = (localStorage.getItem("accessTokenExpiresOn") < new Date().getTime() / 1000) && (localStorage.getItem("accessToken") !== null) 
+  useEffect(() => {
+    if (isTokenExpired && localRefreshToken) {
+      dispatch(refreshToken(localStorage.getItem("refreshToken")));
+    }
+  }, []);
+  console.log("Token expired?", isTokenExpired);
+  if (!localRefreshToken) return <Redirect to="/login"/>;
+
+
+  if (refreshStatus !== "success" && isTokenExpired) {
+    return <div>Bitch I'm refreshing your token!</div>
+  }
+
+  return <Route
+    {...rest}
+    render={(props) =>
+      !isTokenExpired || refreshStatus === "success" ? ( // check whether the access token is expired (that means we're fucked.)
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login/" />
+      )
+    }
+  />;
+};
 
 function App() {
   return (
-   <Router>
-     <Switch>
-       <Route path="/login">
-        <LoginPage />
-       </Route>
-     </Switch>
-   </Router>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <PrivateRoute path="/" component={HomePage} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../state/actions/user.actions";
 
 const LoginPage = () => {
+  const dispatch = useDispatch();
+  const loginStatus = useSelector((state) => state.authentication.status);
+  const loginError = useSelector((state) => state.authentication.error);
+  const loginAssets = useSelector((state) => state.authentication.loginAssets);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  let onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login(username, password));
+  };
+
+  // TODO: Rethink about this one, maybe it should be somewhere else
+  // more generic? for example work in other places of the code globally
+  // TODO: Rethink about the security of the matter, localStorage bad cookies good :)
+  useEffect(() => {
+    if (loginStatus === "success") {
+      localStorage.setItem("refreshToken", loginAssets.refreshToken);
+      localStorage.setItem("accessToken", loginAssets.accessToken);
+      localStorage.setItem("accessTokenExpiresOn", loginAssets.expiresOn);
+
+      document.location.href = "/";
+    }
+  }, [loginStatus]);
+
   return (
     <div class="flex h-screen bg-gray-800">
       <div className="m-auto" style={{ marginTop: "30vh" }}>
-        <p className="text-xl font-bold text-white">
-          Netsle Dashboard.
-        </p>
-        <form className="bg-white shadow-md rounded px-32 pt-6 pb-8 mb-4">
+        <p className="text-xl font-bold text-white">Netsle Dashboard.</p>
+        <form
+          className="bg-white shadow-md rounded px-32 pt-6 pb-8 mb-4"
+          onSubmit={onSubmit}
+        >
           <div>
             <label
               class="block text-gray-700 text-sm font-bold mb-2"
@@ -20,6 +49,7 @@ const LoginPage = () => {
               id="username"
               type="text"
               placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
             ></input>
             <label
               class="block text-gray-700 text-sm font-bold mb-2"
@@ -32,12 +62,24 @@ const LoginPage = () => {
               id="password"
               type="text"
               placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
             ></input>
-            <button className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 mt-4 rounded">
+            <button
+              className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 mt-4 rounded"
+              type="submit"
+            >
               submit
             </button>
           </div>
         </form>
+        {
+          loginStatus === "error" && (
+            <div class="shadow-md bg-red-500 rounded border-2 border-red-400 text-white">
+              <p class="font-bold text-xl">{loginError}</p>
+            </div>
+          )
+        }
+
       </div>
     </div>
   );
